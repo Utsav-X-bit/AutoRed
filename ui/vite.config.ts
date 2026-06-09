cd2eb51794b10f87f2cd143551ed5bd50acab2b1
Utsav Gupta <utsavgupta5467@gmail.com>
2026-06-09 23:39:17 +0530

    chore: change Vite proxy port from 8000 to 8001

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -6,9 +6,9 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:8000',
+      '/api': 'http://localhost:8001',
       '/ws': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8001',
         ws: true,
       },
     },
