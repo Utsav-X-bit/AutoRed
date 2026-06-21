e28f08d84ed3867e5f139631743aa4e6abcb10a5
isea11 <isea11@login.npsf.cdac.in>
2026-06-21 17:27:25 +0530

    Cloud Commit

--- experiment/llama_3_8b_verbose.py
+++ experiment/llama_3_8b_verbose.py
@@ -76,9 +76,9 @@ LLAMA_PATH = "meta-llama/Meta-Llama-3-8B-Instruct"
 GENERATOR_PATH = "Orenguteng/Llama-3.1-8B-Lexi-Uncensored-V2"
 BASE_GENERATOR_PATH = "Orenguteng/Llama-3.1-8B-Lexi-Uncensored-V2"
 
-DISTILBERT_CKPT = "/nlsasfs/home/isea/isea13/AutoRed/pre_trained/pi_reward_model"
+DISTILBERT_CKPT = "/nlsasfs/home/isea/isea11/slurmJobs/AutoRed/pre_trained/pi_reward_model"
 
-DATA_PATH = "/nlsasfs/home/isea/isea13/AutoRed/experiment/raw_dump_defenses.jsonl.bz2"
+DATA_PATH = "/nlsasfs/home/isea/isea11/slurmJobs/AutoRed/experiment/raw_dump_defenses.jsonl.bz2"
 EXT_DATA_PATH = "scripts/pi/pi_data/pi_ext_data/train.json"
 
 # Where to save the full trace log
