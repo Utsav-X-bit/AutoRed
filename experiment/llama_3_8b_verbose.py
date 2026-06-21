6ff5f2a52f44dc22a8bfcc4ad6d4673a6a71b9f5
Utsav Gupta <utsavgupta5467@gmail.com>
2026-06-21 11:33:55 +0530

    local commit

--- experiment/llama_3_8b_verbose.py
+++ experiment/llama_3_8b_verbose.py
@@ -329,7 +329,7 @@ def load_gen_model(ckpt_path: str, base_model_path: str = BASE_GENERATOR_PATH):
             torch_dtype=torch.float16,
             device_map={"": device},
             local_files_only=True,
-            load_in_4bit=True,
+            quantization_config=BitsAndBytesConfig(load_in_4bit=True),
         )
         model = PeftModel.from_pretrained(base_model, ckpt_path, local_files_only=True)
     else:
